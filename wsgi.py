361676679d6423592f1894c2794d5f3dcbe738af
simultech <uqadekke@snrswdev.ceit.uq.edu.au>
2014-03-06 11:32:22 +1000

    Prod settings

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ import sys
 APP_BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(APP_BASE_DIR)
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "grapher.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = "grapher.settings_production"
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
