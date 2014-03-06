deeff3e072c5bd3e6edaf76883ff15c626fc5e77
simultech <uqadekke@snrswdev.ceit.uq.edu.au>
2014-03-06 10:19:40 +1000

    Changing paths

--- wsgi.py
+++ wsgi.py
@@ -6,6 +6,9 @@ APP_BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Add the app's directory to the PYTHONPATH
 sys.path.append(APP_BASE_DIR)
 sys.path.append(os.path.join(APP_BASE_DIR, 'grapher'))
+sys.path.append('/var/www/html/xygrapher/src/uqx-xygrapher')
+sys.path.append('/var/www/html/xygrapher/src/uqx-xygrapher/xygrapher')
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "grapher.settings")
 
