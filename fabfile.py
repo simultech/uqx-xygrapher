02972bb53c7cc7a56d6eecdee704132ca1522d25
simultech <uqadekke@snrswdev.ceit.uq.edu.au>
2014-03-12 11:17:38 +1000

    Auto Commit

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,7 @@
 from fabric.api import *
+from fabric.contrib.console import confirm
 
+verbose = True
 env.projectname = 'grapher'
 env.activate = 'source /Users/uqadekke/Sites/sources/grapher/env/bin/activate'
 
@@ -8,12 +10,30 @@ def host_type():
     run('uname -s')
 
 def prepare_deploy():
-    git_message = "Auto Commit"
-    print("Git Push and Commit")
+    func_test()
+    func_gitadd("Auto Commit")
+    func_gitpush()
+
+#Internal
+
+def func_test():
+    if verbose:
+        print "Testing project"
     local_ve("./manage.py test "+env.projectname)
+
+def func_gitadd(git_message):
+    if verbose:
+        print "Git adding"
     local_ve("git add . && git commit -a -m \""+git_message+"\"")
+
+def func_gitpush():
+    if verbose:
+        print "Pushing to github"
     local_ve("git push")
 
+#Helpers
+
 def local_ve(cmd):
-    print "DOING: "+cmd
-    local(env.activate+" && "+cmd)
+    result = local(env.activate+" && "+cmd)
+    if result.failed and not confirm("Command failed. Continue anyway?"):
+        abort("Aborting at user request.")
