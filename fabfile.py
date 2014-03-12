b057b802eb53fcab978b8dc5388f4ba8d14ca17f
simultech <uqadekke@snrswdev.ceit.uq.edu.au>
2014-03-12 11:43:59 +1000

    Auto Commit

--- fabfile.py
+++ fabfile.py
@@ -3,34 +3,59 @@ from fabric.contrib.console import confirm
 
 verbose = True
 env.projectname = 'grapher'
-env.activate = 'source /Users/uqadekke/Sites/sources/grapher/env/bin/activate'
+env.git_projectname = 'uqx-xygrapher'
+env.remote_projectname = 'xygrapher'
 
-def host_type():
-    print("Hello world!")
-    run('uname -s')
+env.local_base = '/Users/uqadekke/Sites/sources/grapher'
+env.remote_base = '/var/www/django/'+env.remote_projectname
+env.remote_code_dir = env.remote_base+'/src/'+env.git_projectname
 
-def prepare_deploy():
+env.hosts = ['uqmarkup']
+
+def prepare():
     func_test()
     func_gitadd("Auto Commit")
     func_gitpush()
 
+def deploy():
+    with settings(warn_only=True):
+        if run("test -d %s" % env.remote_code_dir).failed:
+            print "NOT CLONED YET"
+            #run("git clone user@vcshost:/path/to/repo/.git %s" % env.remote_code_dir)
+    with cd(env.remote_code_dir):
+        remote_vc("git pull","Pulling from git")
+        #run("touch app.wsgi")
+
 #Internal
 
 def func_test():
-    local_ve("./manage.py test "+env.projectname, "Testing project")
+    local_ve("./manage.py test " + env.projectname, "Testing project")
+
 
 def func_gitadd(git_message):
-    local_ve("git add . && git commit -a -m \""+git_message+"\"", "Git adding")
+    local_ve("git add . && git commit -a -m \"" + git_message + "\"", "Git adding")
+
 
 def func_gitpush():
     local_ve("git push", "Pushing to github")
 
+
 #Helpers
 
-def local_ve(cmd,message):
+def local_ve(cmd, message):
     if verbose:
-        print "Command: "+message
-    with hide('output','running','warnings'), settings(warn_only=True):
-        result = local(env.activate+" && "+cmd, capture=True)
-        if result.failed and not confirm("+ Error: "+message+" failed. Continue anyway?"):
+        print "Command: " + message
+    with hide('output', 'running', 'warnings'), settings(warn_only=True):
+        envcmd = 'source '+env.local_base+'/env/bin/activate'
+        result = local(envcmd + " && " + cmd, capture=True)
+        if result.failed and not confirm("+ Error: " + message + " failed. Continue anyway?"):
             abort("Aborting at user request.")
+
+def remote_vc(cmd, message):
+    if verbose:
+        print "Command: " + message
+    #with hide('output', 'running', 'warnings'), settings(warn_only=True):
+    envcmd = 'source '+env.remote_base+'/env/bin/activate'
+    result = sudo(envcmd + " && " + cmd)
+    if result.failed and not confirm("+ Error: " + message + " failed. Continue anyway?"):
+        abort("Aborting at user request.")
